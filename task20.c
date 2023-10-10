6daad846440b811a78f282dc248fb5b44dd15b87
jahanzaib-ahsan <mail.jahanzaibahsan@gmail.com>
2023-10-10 12:25:26 +0500

    Arrays

---
+++ task20.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int marks [4];
+    marks[0]=34;
+    printf("Marks of student 1 is %d\n", marks[0] );
+    marks[1]=44;
+    printf("Marks of student 2 is %d\n", marks[1] );
+    marks[2]=54;
+    printf("Marks of student 3 is %d\n", marks[2] );
+    marks[3]=64;
+    printf("Marks of student 4 is %d\n", marks[3] );
+    return 0;
+}
