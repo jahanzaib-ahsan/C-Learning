5dcc795648fdf488e6f182e9b85d2bfbd23bd1c9
jahanzaib-ahsan <mail.jahanzaibahsan@gmail.com>
2023-10-13 21:37:17 +0500

    arithmetic pointers

---
+++ task27.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main()
+{
+    char a = '32';
+    char* ptra = &a;
+    printf("%d\n", ptra);
+    ptra++;
+    printf("%d\n", ptra);
+    printf("%d", ptra-2);
+    return 0;
+}
